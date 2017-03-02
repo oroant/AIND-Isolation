d0b51c3ea2d28d35946fdaf16468a8654227c557
Antonio Oro <antonio.oro@bluewin.ch>
2017-03-02 17:04:54 +0100

    first version of heuristics, minimax, alphabeta implemented

--- game_agent.py
+++ game_agent.py
@@ -6,14 +6,31 @@ augment the test suite with your own test cases to further test your code.
 You must test your agent's strength against a set of agents with known
 relative strength using tournament.py and include the results in your report.
 """
+
 import random
+import heuristics as hs
 
 
 class Timeout(Exception):
     """Subclass base exception for code clarity."""
     pass
 
+def cached_score(fn):
+    """ cache scores from function to avoid calculation penalty """
 
+    cache = {}
+
+    def score(game, player):
+        key = game.to_string()
+
+        if key not in cache:
+            cache[key] = fn(game, player)
+
+        return cache[key]
+
+    return score
+
+@cached_score
 def custom_score(game, player):
     """Calculate the heuristic value of a game state from the point of view
     of the given player.
@@ -37,8 +54,7 @@ def custom_score(game, player):
         The heuristic value of the current game state to the specified player.
     """
 
-    # TODO: finish this function!
-    raise NotImplementedError
+    return hs.ratio_count_score(game, player)
 
 
 class CustomPlayer:
@@ -117,26 +133,32 @@ class CustomPlayer:
         """
 
         self.time_left = time_left
+        result = (-1, -1)
+        depth = 1
 
-        # TODO: finish this function!
-
-        # Perform any required initializations, including selecting an initial
-        # move from the game board (i.e., an opening book), or returning
-        # immediately if there are no legal moves
+        if len(legal_moves) == 0:
+            return result
 
         try:
-            # The search method call (alpha beta or minimax) should happen in
-            # here in order to avoid timeout. The try/except block will
-            # automatically catch the exception raised by the search method
-            # when the timer gets close to expiring
-            pass
+            if self.iterative:
+                while True:
+                    _, result = self.apply_method(game, depth)
+                    depth += 1
+            else:
+                return self.apply_method(game, self.search_depth)[1]
 
         except Timeout:
-            # Handle any actions required at timeout, if necessary
-            pass
+            return result
 
-        # Return the best move from the last completed search iteration
-        raise NotImplementedError
+
+    def apply_method(self, game, depth):
+        """ Apply game algorithm based on setting passed in during initialization """
+
+        if self.method == "minimax":
+            return self.minimax(game, depth)
+        else:
+            return self.alphabeta(game, depth)
+
 
     def minimax(self, game, depth, maximizing_player=True):
         """Implement the minimax search algorithm as described in the lectures.
@@ -172,8 +194,29 @@ class CustomPlayer:
         if self.time_left() < self.TIMER_THRESHOLD:
             raise Timeout()
 
-        # TODO: finish this function!
-        raise NotImplementedError
+        legal_moves = game.get_legal_moves()
+
+        if len(legal_moves) == 0 or depth == 0:
+            return self.score(game, self), (-1, -1)
+
+        def map_outcome(move):
+            max_player = not maximizing_player
+            new_board = game.forecast_move(move)
+            score, _ = self.minimax(new_board, depth - 1, max_player)
+            return {"score": score, "move": move}
+
+        outcomes = [map_outcome(move) for move in legal_moves]
+
+        result = None
+        get_score = lambda x: x["score"]
+
+        if maximizing_player:
+            result = max(outcomes, key=get_score)
+        else:
+            result = min(outcomes, key=get_score)
+
+        return result["score"], result["move"]
+
 
     def alphabeta(self, game, depth, alpha=float("-inf"), beta=float("inf"), maximizing_player=True):
         """Implement minimax search with alpha-beta pruning as described in the
@@ -216,5 +259,42 @@ class CustomPlayer:
         if self.time_left() < self.TIMER_THRESHOLD:
             raise Timeout()
 
-        # TODO: finish this function!
-        raise NotImplementedError
+        legal_moves = game.get_legal_moves()
+        current_move = (-1, -1)
+
+        if len(legal_moves) == 0 or depth == 0:
+            return self.score(game, self), current_move
+        else:
+            current_move = legal_moves[0]
+
+        if maximizing_player:
+            current = float("-inf")
+
+            for move in legal_moves:
+                score, _ = self.alphabeta(game.forecast_move(move),
+                                          depth - 1, alpha, beta, False)
+
+                if score > current:
+                    current_move = move
+
+                current = max(current, score)
+                alpha = max(alpha, current)
+
+                if beta <= alpha:
+                    break
+        else:
+            current = float("inf")
+
+            for move in legal_moves:
+                score, _ = self.alphabeta(game.forecast_move(move),
+                                          depth - 1, alpha, beta, True)
+
+                if score < current:
+                    current_move = move
+
+                current = min(current, score)
+                beta = min(beta, current)
+
+                if beta <= alpha:
+                    break
+        return current, current_move
---
+++ heuristics.py
@@ -0,0 +1,64 @@
+"""
+
+This file contains all the various heuristics for the Isolation game.
+
+"""
+
+
+def baseline_score(game, player):
+    """ returns the simple heuristic score as difference of moves"""
+
+    other_player = game.get_opponent(player)
+    our_moves = len(game.get_legal_moves(player))
+    their_moves = len(game.get_legal_moves(other_player))
+
+    return game.utility(player) + float(our_moves - their_moves)
+
+
+def alternative_baseline_score(game, player):
+    """ returns alternative baseline score, weighting their moves """
+
+    other_player = game.get_opponent(player)
+    our_moves = len(game.get_legal_moves(player))
+    their_moves = len(game.get_legal_moves(other_player))
+
+    return game.utility(player) + our_moves - (2.0 * their_moves)
+
+
+def ratio_score(game, player):
+    """ returns score as a ratio of valid moves to blank spaces """
+
+    blank_count = len(game.get_blank_spaces())
+    result = baseline_score(game, player)
+
+    if blank_count > 0:
+        result = (result / blank_count)
+
+    return result
+
+
+def ratio_count_score(game, player):
+    """ returns ratio score from available moves by difference"""
+
+    other_player = game.get_opponent(player)
+    our_moves = len(game.get_legal_moves(player))
+    their_moves = len(game.get_legal_moves(other_player))
+
+    diff = float(our_moves - their_moves)
+
+    if diff != 0:
+        return game.utility(player) + (game.move_count / diff)
+    else:
+        return game.utility(player)
+
+
+def ratio_blank_score(game, player):
+    """ available legal moves compared to blank spaces """
+
+    blank_count = len(game.get_blank_spaces())
+    result = game.utility(player)
+
+    if blank_count > 0:
+        result = result + (len(game.get_legal_moves(player)) / blank_count)
+
+    return result
